2b3da15c0c2ea7125cc0743054028639cffa921d
Chet Corcos <ccorcos@gmail.com>
2017-04-19 22:43:26 -0700

    hcl viz

--- shader.glsl
+++ shader.glsl
@@ -1,10 +1,54 @@
-// HSL colorspace:
-// https://www.shadertoy.com/view/XljGzV
-vec3 hsl2rgb( in vec3 c )
-{
-    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
+// HCL colorspace:
+// https://www.shadertoy.com/view/Xt3SDs
+const float X = 0.950470;
+const float Y = 1.0;
+const float Z = 1.088830;
 
-    return c.z + c.y * (rgb-0.5)*(1.0-abs(2.0*c.z-1.0));
+const float t0 = 4.0 / 29.0;
+const float t1 = 6.0 / 29.0;
+const float t2 = 3.0 * t1 * t1;
+const float t3 = t1 * t1 * t1;
+
+float lab_xyz(float t) {
+    return t > t1 ? t * t * t : t2 * (t - t0);
+}
+
+float xyz_rgb(float x) {
+    return x <= 0.0031308 ? 12.92 * x : 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+}
+
+vec3 lab2rgb(vec3 lab) {
+    float l = lab.x;
+    float a = lab.y;
+    float b = lab.z;
+    float y = (l + 16.0) / 116.0;
+    float x = y + a / 500.0;
+    float z = y - b / 200.0;
+    x = lab_xyz(x) * X;
+    y = lab_xyz(y) * Y;
+    z = lab_xyz(z) * Z;
+    return vec3(
+        xyz_rgb( 3.2404542 * x - 1.5371385 * y - 0.4985314 * z),
+		xyz_rgb(-0.9692660 * x + 1.8760108 * y + 0.0415560 * z),
+        xyz_rgb( 0.0556434 * x - 0.2040259 * y + 1.0572252 * z)
+    );
+}
+
+vec3 hcl2lab(vec3 hcl) {
+    float h = hcl.x;
+    float c = hcl.y;
+    float l = hcl.z;
+    h = radians(h);
+    return vec3(l, cos(h) * c, sin(h) * c);
+}
+
+vec3 hcl2rgb(vec3 hcl) {
+    return lab2rgb(hcl2lab(hcl));
+}
+
+vec3 hcl(float h, float c, float l)
+{
+	return hcl2rgb(vec3(h * 360.0, c * 128.0, l * 100.0));
 }
 
 
@@ -61,6 +105,6 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         + spin * rel.x * rel.y / res.x / res.y * tightness;
 
     // Compute rgb:
-    vec3 rgb = hsl2rgb(vec3(hue, 1.0, 0.6));
+    vec3 rgb = hcl(hue, 0.66, 0.76);
     fragColor = vec4(rgb, 1.0);
 }
